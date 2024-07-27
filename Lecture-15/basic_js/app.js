d5632aad9b8702d0a64f121628ba401f338e1c1b
Samarth <officialdevelopersamarth@gmail.com>
2024-07-27 15:27:20 +0530

    lecture 15 codes

---
+++ Lecture-15/basic_js/app.js
@@ -0,0 +1,18 @@
+// let a = 100;
+// console.log(a)
+
+
+// ---------------
+
+// let abc = "sam"
+// // let abc = "lol" //redeclaration ❌
+
+// abc = "lol" //reinitialise ✅
+// abc =  10
+// abc =  true
+// abc =  false
+
+// console.log(abc)
+
+// ---------------
+
---
+++ Lecture-15/strings/app.js
@@ -0,0 +1,9 @@
+let a = "sam"
+console.log(a)
+
+
+let b = 'sam'
+console.log(b)
+
+let c = `sam`
+console.log(c)
