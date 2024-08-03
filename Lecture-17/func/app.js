ed38c1dc58e547d31766b9d20cd9e1f94a494127
Samarth <officialdevelopersamarth@gmail.com>
2024-08-03 15:18:47 +0530

    lecture 17 codes

---
+++ Lecture-17/func/app.js
@@ -0,0 +1,77 @@
+
+//func  defi
+// function kuchbhi(){
+//     console.log("kuch bhi chal gya...")
+// }
+// function bhaubhau(){
+//     console.log("bhau bhau chal gya...")
+//     return "tommy"
+// }
+//fn calling
+// let returnedVal = kuchbhi();
+// let returnedVal2 = bhaubhau();
+// console.log(returnedVal)
+// console.log(returnedVal2)
+
+
+// ---------------------------
+// //parameterised fn
+// function square(baba){ //parameter
+//     return baba*baba
+// }
+// console.log(square(6)) 
+
+// let out = square(5) //arguments
+// console.log(out)
+
+// ------------------------
+
+//default parameterised fn
+// function add(a=100 , b=420){ 
+//     return a+b
+// }
+
+
+// let out = add(5) 
+// // console.log(typeof(out)) 
+// console.log(out)
+
+// let a = 1;
+// let b = true;
+// let c = "1";
+// let d = null; // object
+// let e = undefined;
+// let f = -Infinity;
+// let g = +Infinity;
+// let h = NaN; //number
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof f)
+// console.log(typeof g)
+// console.log(typeof h)
+
+
+// -------------------------
+
+
+// let karsakteho = function lol (){
+//     console.log("i am coding blocks")
+// }
+// karsakteho()
+// // lol() ❌
+
+// -------------------------
+
+// method
+// function
+
+
+
+
+
+
+
---
+++ Lecture-17/loops/app.js
@@ -0,0 +1,44 @@
+
+
+// let arr = [1,2,3];
+
+// let obj = {
+//     a: 10,
+//     b: "sam",
+// }
+
+// // for-of (array)
+// // for(let item of arr){
+// //     console.log(item) //every element 1-1 
+// // }
+
+
+// // for-in (object)
+// for(let item in obj){
+//     // console.log(item)
+//     // console.log(obj.item)
+//     console.log(obj[item])
+//     console.log(obj["item"])
+// }
+
+// --------------
+
+
+let obj = {
+    naam : "baba benagali",
+    kaam : "vashikaran",
+    number : 9567080335 , 
+//methods
+    baba : function (){
+        console.log("mai hu gian")
+    }
+}
+
+// console.log(console)
+console.log(obj.number)
+console.log(obj.kaam)
+console.log(obj.baba)
+
+console.log(obj.baba())
+console.log(10)
+
