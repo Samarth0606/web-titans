778682855ceb3fa2fb6cce021209f4feb5f92279
Samarth <officialdevelopersamarth@gmail.com>
2024-09-21 15:25:46 +0530

    lecture 30 codes

---
+++ Lecture-30/node-basic/app.js
@@ -0,0 +1,16 @@
+// console.log(process.argv);
+// // console.log("10")
+
+
+// let inp = process.argv[2];
+
+// for(let i = 1 ; i<= inp ; i++){
+//     console.log(i)
+// }
+
+
+console.log(process.cwd()); // lesser
+console.log(__dirname) //entire
+
+// /Users/macbook/Documents/Classes/web-titans
+// /Users/macbook/Documents/Classes/web-titans/Lecture-30/node-basic
